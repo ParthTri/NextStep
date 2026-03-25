90dbf19e77d5f7c8d8859060a83c0f7cea36c85d
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2026-03-26 12:27:45 +1300

    Adding tables for tags

--- src/nextstep/models.py
+++ src/nextstep/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    colour = models.CharField(max_length=8, null=False, default="3B82F6")
+
+    def __str__(self):
+        return self.name
 
 
 class Application(models.Model):
@@ -14,3 +21,16 @@ class Application(models.Model):
     applied_timestamp = models.DateTimeField(null=False, default=timezone.now)
 
     job_description = models.TextField(null=False, blank=True)
+
+    tags = models.ManyToManyField(Tag, through="ApplicationTag", related_name="tags")
+
+
+class ApplicationTag(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # auto_now updates the field every time the save() method is called
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # Prevents the same tag being added to the same application twice
+        unique_together = ("application", "tag")
