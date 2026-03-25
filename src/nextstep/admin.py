552d26c068cc9ec27d47a865bd573b77aa247374
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2026-03-26 12:52:23 +1300

    Registering models onto admin site

--- src/nextstep/admin.py
+++ src/nextstep/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from nextstep import models
+
 # Register your models here.
+
+admin.site.register(models.Tag)
+admin.site.register(models.Application)
+admin.site.register(models.ApplicationTag)
