db7bdff6678685c001a6d3265fde2d5ba6a7d448
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2026-03-26 10:58:07 +1300

    Passing form into context for dashboard view

--- src/nextstep/views.py
+++ src/nextstep/views.py
@@ -1,6 +1,15 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+
+from nextstep import forms, models
 
 
 # Create your views here.
-def dashboard(request):
-    return render(request, "dashboard.html")
+def dashboard(request: HttpRequest):
+    context = {}
+
+    application_form = forms.ApplicationForm()
+
+    context["application_form"] = application_form
+
+    return render(request, "dashboard.html", context)
