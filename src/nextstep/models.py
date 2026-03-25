1dc1753bc45d87e3dc6db23afb0bd83322237df4
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2026-03-26 10:04:26 +1300

    Creating first model

--- src/nextstep/models.py
+++ src/nextstep/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Application(models.Model):
+    role = models.CharField(null=False, blank=False)
+    role_link = models.CharField(null=False, blank=True)
+
+    company = models.CharField(null=False, blank=False)
+    company_link = models.CharField(null=False, blank=True)
+
+    applied_timestamp = models.DateTimeField(null=False, default=timezone.now)
+
+    job_description = models.TextField()
